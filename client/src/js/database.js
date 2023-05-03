6d29163f992af2cc5ea1a8c773ec384f32d984ae
Cassie LeClair <cassieleclair71@gmail.com>
2023-05-02 20:13:55 -0400

    name change

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,22 +1,22 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('texttinker', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('texttinker')) {
+        console.log('texttinker database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('texttinker', { keyPath: 'id', autoIncrement: true });
+      console.log('texttinker database created');
     },
   });
 
 // Method to accept content and add it to the database
 export const putDb = async (content) => {
   const db = await initdb();
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const tx = db.transaction('texttinker', 'readwrite');
+  const store = tx.objectStore('texttinker');
   await store.put({ content });
   await tx.done;
   console.log('Content added to the database');
@@ -25,8 +25,8 @@ export const putDb = async (content) => {
 // logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await initdb();
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
+  const tx = db.transaction('texttinker', 'readonly');
+  const store = tx.objectStore('texttinker');
   const allContent = await store.getAll();
   await tx.done;
   console.log('Content retrieved from the database');
